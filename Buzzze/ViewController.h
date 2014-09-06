9692bedc2d20298a2f2a324b1279f0f36c10e0ad
Mohtashim Khan <mohkhan@yahoo-inc.com>
2014-09-06 16:20:45 -0700

    Initial commit

---
+++ Buzzze/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Buzzze
+//
+//  Created by Mohtashim Khan on 8/3/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Buzzze/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Buzzze
+//
+//  Created by Mohtashim Khan on 8/3/14.
+//  Copyright (c) 2014 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
